ceb2403b4134896b32e0cd2fb22a6fa50cca4b5f
KHYLE <KHYLE@KHYLE-HP>
2021-05-26 13:09:21 -0400

    Testing

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+import java.util.Scanner;
+
+/**
+ *
+ * @author KHYLE
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */        
+           
+    public static void main(String[] args) {
+        String name;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter a string: ");
+        name=input.nextLine();
+        
+        char[] revName=name.toCharArray();
+        
+        for(int i= revName.length-1; i>=0;i--)
+        {
+            System.out.print(revName[i]);
+        }
+    }
+
+}
+            
+        
+    
+    
+
